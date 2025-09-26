235ce974324dc0d6ac8f5bb4b08015f3c1949ec8
SilvaMurilo <contato@silvamurilo.com.br>
2025-09-25 22:09:47 -0300

    fix: database configuration to use createPool from mysql2 and update connection parameters

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,17 @@
-import { Kysely, MysqlDialect } from 'kysely'
-import mysql from 'mysql2/promise'
+import { createPool } from "mysql2";
+import { Kysely, MysqlDialect } from "kysely";
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST ?? '127.0.0.1',
-  user: process.env.DB_USER ?? 'root',
-  password: process.env.DB_PASS ?? '',
-  database: process.env.DB_NAME ?? 'ozmap_challenge',
-  connectionLimit: 10,
-})
+const dialect = new MysqlDialect({
+  pool: createPool({
+    database: "ozmap",
+    host: "127.0.0.1",
+    user: "desafio",
+    password: "desafio",
+    port: 3306,
+    connectionLimit: 10,
+  }),
+});
 
 export const db = new Kysely<any>({
-  dialect: new MysqlDialect({ pool }),
-})
+  dialect,
+});
