9c3a3c8182975a19b0d409113172637fd68df5c1
SilvaMurilo <contato@silvamurilo.com.br>
2025-09-28 10:37:46 -0300

    refactor: update createCable function to accept DropCableIsp type

--- src/ozSdk.ts
+++ src/ozSdk.ts
@@ -2,7 +2,7 @@ import OZMapSDK from "@ozmap/ozmap-sdk";
 // import type { Cable, CreateCableDTO } from "@ozmap/ozmap-sdk";
 import axios from "axios";
 import { CableOZmap } from "./schemas/ozmapSchema";
-import { CableIsp } from "./schemas/ispSchema";
+import { CableIsp, DropCableIsp } from "./schemas/ispSchema";
 
 const OZMAP_BASE_URL = process.env.OZMAP_BASE_URL ?? "http://localhost:9994/api/v2";
 const OZMAP_API_KEY  = process.env.OZMAP_API_KEY  ?? "TEST";
@@ -10,7 +10,7 @@ export const oz: any = new (OZMapSDK as any)(OZMAP_BASE_URL, { apiKey: OZMAP_API
 
 
 
-export async function createCable(body: CableIsp){  
+export async function createCable(body: CableIsp | DropCableIsp) {  
     const res = await axios.post(`${OZMAP_BASE_URL}/cables`, body, {
       headers: {
         "Content-Type": "application/json"
